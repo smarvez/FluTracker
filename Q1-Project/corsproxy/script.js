210544bb93fb708197e345722617d1c126eb23ae
Stephanie Marvez <marvez@gmail.com>
2017-10-30 11:27:07 -0600

    slider has some functionality but not to specific days

--- Q1-Project/corsproxy/script.js
+++ Q1-Project/corsproxy/script.js
@@ -25,32 +25,47 @@ $(document).ready(function() {
 
       // add markers to map
       // function addMarkers() {
-        for (var i = 0; i < data.length; i++) {
-          var latitude = data[i].latitude;
-          var longitude = data[i].longitude;
-          var tweet = data[i].tweet_text;
-          var image = "Sick_Emoji.png";
-          var latLng = new google.maps.LatLng(latitude, longitude);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            icon: image
-          });
-          marker.setMap(map);
-          attachSecretMessage(marker, tweet);
-        }
+        // for (var i = 0; i < data.length; i++) {
+        //   var latitude = data[i].latitude;
+        //   var longitude = data[i].longitude;
+        //   var tweet = data[i].tweet_text;
+        //   var image = "Sick_Emoji.png";
+        //   var latLng = new google.maps.LatLng(latitude, longitude);
+        //   var marker = new google.maps.Marker({
+        //     position: latLng,
+        //     map: map,
+        //     icon: image
+        //   });
+        //   marker.setMap(map);
+        //   attachSecretMessage(marker, tweet);
+        // }
       // }
 
       // find date groups
       var start = Date.now();
       var today = new Date(start);
-      console.log(today);
+      var todaysDate = today.getDate();
+      console.log(todaysDate);
 
-      for (var j = 0; j < data.length; j++) {
-        var date = new Date(data[j].tweet_date * 1000);
-        var day = date.getDate();
-
-      }
+      // for (var j = 0; j < data.length; j++) {
+      //   var latitude = data[j].latitude;
+      //   var longitude = data[j].longitude;
+      //   var tweet = data[j].tweet_text;
+      //   var image = "Sick_Emoji.png";
+      //   var date = new Date(data[j].tweet_date * 1000);
+      //   var day = date.getDate();
+      //   var daysAgo = (todaysDate - day);
+      //   var latLng = new google.maps.LatLng(latitude, longitude);
+      //   var marker = new google.maps.Marker({
+      //     position: latLng,
+      //     map: map,
+      //     icon: image
+      //   });
+      //   if (daysAgo === 0) {
+      //     marker.setMap(map);
+      //     attachSecretMessage(marker, tweet);
+      //   }
+      // }
 
       // add event listenter to markers to get tweets
       function attachSecretMessage(marker, secretMessage) {
@@ -66,18 +81,35 @@ $(document).ready(function() {
         })
       }
       $( function() {
-    $( "#slider" ).slider({
-      range: "max",
-      min: 0,
-      max: 7,
-      value: 3,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
-  } );
-
-
+        $( "#slider" ).slider({
+          range: "max",
+          min: 0,
+          max: 7,
+          value: 3,
+          change: function( event, ui ) {
+            $( "#amount" ).val( ui.value );
+            for (var j = 0; j < data.length; j++) {
+              var latitude = data[j].latitude;
+              var longitude = data[j].longitude;
+              var tweet = data[j].tweet_text;
+              var image = "Sick_Emoji.png";
+              var date = new Date(data[j].tweet_date * 1000);
+              var day = date.getDate();
+              var daysAgo = (todaysDate - day);
+              var latLng = new google.maps.LatLng(latitude, longitude);
+              if (daysAgo === 0) {
+                var marker = new google.maps.Marker({
+                  position: latLng,
+                  map: map,
+                  icon: image
+                });
+                marker.setMap(map);
+                attachSecretMessage(marker, tweet);
+              }
+            }
+          }
+        });
+        $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+      } );
     })
 })
